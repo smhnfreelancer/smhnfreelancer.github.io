061f10824794cdf27e149195b5235016189c75a0
SMHN <smhnfreelancer@gmail.com>
2024-04-11 22:21:25 +0330

    Highlight Active link

--- flixx/js/script.js
+++ flixx/js/script.js
@@ -1,12 +1,41 @@
 const global = {
-  currentPage: window.location.pathname
+  currentPage: window.location.pathname,
 };
+
+// Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  if (global.currentPage === '/flixx/') {
+    global.currentPage = '/flixx/index.html';
+  }
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
 
 // Init App
 function init() {
   switch (global.currentPage) {
-    case '/':
+    case '/flixx/':
+    case '/flixx/index.html':
       console.log('Home');
       break;
+    case '/flixx/shows.html':
+      console.log('Shows');
+      break;
+    case '/flixx/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/flixx/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
   }
+
+  highlightActiveLink();
 }
+document.addEventListener('DOMContentLoaded', init);
