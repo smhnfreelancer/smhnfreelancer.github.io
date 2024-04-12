1ed68b5d378c8f5dbdffc27054165dc1d295ffbd
SMHN <smhnfreelancer@gmail.com>
2024-04-12 09:15:43 +0330

    display popular

--- flixx/js/script.js
+++ flixx/js/script.js
@@ -2,6 +2,53 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+  console.log(results);
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+    ${
+      movie.poster_path
+        ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+    />`
+        : `<img
+    src="images/no-image.jpg"
+    class="card-img-top"
+    alt="${movie.title}"
+    />`
+    }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+      </p>
+    </div>
+    `;
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'bd4f89daf97efe366d57d50a6ec1aad0';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlight active link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -20,7 +67,7 @@ function init() {
   switch (global.currentPage) {
     case '/flixx/':
     case '/flixx/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/flixx/shows.html':
       console.log('Shows');
