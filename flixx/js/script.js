a346f569c467bdc880780ab0757fbbdf84562db0
SMHN <smhnfreelancer@gmail.com>
2024-05-06 14:47:36 +0330

    Movie Details importing data into the html

--- flixx/js/script.js
+++ flixx/js/script.js
@@ -59,10 +59,10 @@ async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
 
   const movie = await fetchAPIData(`movie/${movieId}}`);
-
+  console.log(movie.genres.map((genre) => `<li>${genre.name}</li>`).join(''));
   const div = document.createElement('div');
 
-  div.innerHTML = `  
+  div.innerHTML = `
                   <div class="details-top">
                   <div>
                   ${
@@ -75,37 +75,43 @@ async function displayMovieDetails() {
                     <h2>${movie.title}</h2>
                     <p>
                       <i class="fas fa-star text-primary"></i>
-                      8 / 10
+                      ${movie.vote_average.toFixed(1)} / 10
                     </p>
-                    <p class="text-muted">Release Date: XX/XX/XXXX</p>
+                    <p class="text-muted">Release Date: ${
+                      movie.release_date
+                    }</p>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-                      atque molestiae error debitis provident dolore hic odit, impedit
-                      sint, voluptatum consectetur assumenda expedita perferendis
-                      obcaecati veritatis voluptatibus. Voluptatum repellat suscipit,
-                      quae molestiae cupiditate modi libero dolorem commodi obcaecati!
-                      Ratione quia corporis recusandae delectus perspiciatis consequatur
-                      ipsam. Cumque omnis ad recusandae.
+                     ${movie.overview}
                     </p>
                     <h5>Genres</h5>
                     <ul class="list-group">
-                      <li>Genre 1</li>
-                      <li>Genre 2</li>
-                      <li>Genre 3</li>
+                      ${movie.genres
+                        .map((genre) => `<li>${genre.name}</li>`)
+                        .join('')}
                     </ul>
-                    <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+                    <a href="${
+                      movie.homepage
+                    }" target="_blank" class="btn">Visit Movie Homepage</a>
                   </div>
                 </div>
                 <div class="details-bottom">
                   <h2>Movie Info</h2>
                   <ul>
-                    <li><span class="text-secondary">Budget:</span> $1,000,000</li>
-                    <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
-                    <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
-                    <li><span class="text-secondary">Status:</span> Released</li>
+                    <li><span class="text-secondary">Budget:
+                    </span>$${addCommasToNumber(movie.budget)}</li>
+                    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+                      movie.revenue
+                    )}</li>
+                    <li><span class="text-secondary">Runtime:</span>
+                     ${movie.runtime} minutes</li>
+                    <li><span class="text-secondary">Status:</span> ${
+                      movie.status
+                    }</li>
                   </ul>
                   <h4>Production Companies</h4>
-                  <div class="list-group">Company 1, Company 2, Company 3</div>
+                  <div class="list-group">${movie.production_companies
+                    .map((company) => `<span>${company.name}</span>`)
+                    .join('')}</div>
                 </div>`;
 
   document.querySelector('#movie-details').appendChild(div);
@@ -150,6 +156,10 @@ function highlightActiveLink() {
       link.classList.add('active');
     }
   });
+}
+
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 // Init App
