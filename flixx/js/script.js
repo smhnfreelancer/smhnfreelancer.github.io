e4b483327d83094f1a901fe0880394aba41a88cc
SMHN <smhnfreelancer@gmail.com>
2024-05-09 22:11:23 +0330

    adding next and prev button and total pages

--- flixx/js/script.js
+++ flixx/js/script.js
@@ -5,6 +5,7 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     apiKey: 'bd4f89daf97efe366d57d50a6ec1aad0',
@@ -229,7 +230,12 @@ async function search() {
   global.search.term = urlParams.get('search-term');
 
   if (global.search.term !== '' && global.search.term !== null) {
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
     if (results.length === 0) {
       showAlert('No result found', 'success');
       return;
@@ -237,13 +243,18 @@ async function search() {
 
     displaySearchResults(results);
 
-    document.querySelector('#search-term').value('');
+    document.querySelector('#search-term').value = '';
   } else {
     showAlert('Please enter a search term');
   }
 }
 
 function displaySearchResults(results) {
+  // Clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -276,7 +287,47 @@ function displaySearchResults(results) {
       </p>
     </div>
     `;
+
+    document.querySelector(
+      '#search-results-heading'
+    ).innerHTML = `<h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>`;
+
     document.querySelector('#search-results').appendChild(div);
+  });
+  displayPagination();
+}
+
+// Create and Display Pagination for Search
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+                     <button class="btn btn-primary" id="prev">Prev</button>
+                     <button class="btn btn-primary" id="next">Next</button>
+                     <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+                  `;
+  document.querySelector('#pagination').appendChild(div);
+  // Disable prev button if on first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+  // Disable the next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  // Next page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  // Previous page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
   });
 }
 
@@ -359,7 +410,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
