94b499991e619448167904d730561b6597db9947
smhnfreelancer <smhnfreelancer@gmail.com>
2024-05-29 10:50:33 +0330

    displaynewworkout and displaynewmeal

--- tracalorie/js/app.js
+++ tracalorie/js/app.js
@@ -105,17 +105,42 @@ class CalorieTracker {
     mealEl.innerHTML = `
     <div class="card-body">
     <div class="d-flex align-items-center justify-content-between">
-      <h4 class="mx-1">Breakfast</h4>
+      <h4 class="mx-1">${meal.name}</h4>
       <div
         class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
       >
-        350
+        ${meal.calories}
       </div>
       <button class="delete btn btn-danger btn-sm mx-2">
         <i class="fa-solid fa-xmark"></i>
       </button>
     </div>
   </div>`;
+
+    mealsEl.appendChild(mealEl);
+  }
+
+  _displayNewWorkout(workout) {
+    const workoutsEl = document.getElementById('workout-items');
+    const workoutEl = document.createElement('div');
+    workoutEl.classList.add('card', 'my-2');
+    workoutEl.setAttribute('data-id', workout.id);
+    workoutEl.innerHTML = `
+    <div class="card-body">
+    <div class="d-flex align-items-center justify-content-between">
+    <h4 class="mx-1">${workout.name}</h4>
+    <div
+        class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+      >
+    ${workout.calories}
+    </div>
+    <button class="delete btn btn-danger btn-sm mx-2">
+    <i class="fa-solid fa-xmark"></i>
+    </button>
+    </div>
+    </div>`;
+
+    workoutsEl.appendChild(workoutEl);
   }
 
   _render() {
